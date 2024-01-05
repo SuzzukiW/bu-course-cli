a8a60de86010ed879296689edf24b1237e65beae
suzzukiw <xfu@bu.edu>
2024-01-04 20:26:32 -0500

    Initial commit

--- bu_course_cli/app.py
+++ bu_course_cli/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 import json
 
@@ -44,4 +45,5 @@ def search_courses():
     return jsonify(filtered_courses)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
